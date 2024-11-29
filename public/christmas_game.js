39201b4d53ab359b366660b263a8192641ea3f94
Natasha Mitchko <natasha.mitchko@gmail.com>
2024-11-29 15:32:15 -0500

    conveyor styling and starting game logic

--- public/christmas_game.js
+++ public/christmas_game.js
@@ -8,7 +8,6 @@ function catAction(catEl) {
     }, 100)
 }
 
-
 function dogAction(dogEl) {
     const bite = document.getElementById('dog-bite').content.cloneNode(true);
     dogEl.replaceChildren(bite);
@@ -35,27 +34,82 @@ window.addEventListener('keydown', function(e) {
 });
 
 
-window.addEventListener('load', function() {
-    
-    const screenWidth = window.screen.width;
+// Game Initialization
 
-    function myMove() {
-        let id = null;
-        const elem = document.getElementById("moving");
-        let pos = 0;
-        clearInterval(id);
-        id = setInterval(frame, 10);
-        function frame() {
-          if (pos >= screenWidth - 10) {
-            console.log("end of the screen");
-            clearInterval(id);
-          } else {
-            pos = pos + 2;
-            elem.style.left = pos + 'px';
-          }
-        }
-      }
+function overlayOff() {
+    document.getElementById("overlay").style.display = "None";
+}
 
-      myMove();
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("start-game-button").addEventListener("click", function() {
+        overlayOff();
+        startGame();
+    });
   });
 
+function startGame() {
+    console.log("Game Started")
+    conveyorOn();
+    var elem = document.getElementById("moving");
+    myMove(elem);
+    setTimeout(() => {
+        dispatchObject()
+    }, 150);
+}
+
+function conveyorOn() {
+    document.getElementById("conveyor-belt").classList.add("conveyor-belt-animation");
+}
+
+// TODO: Spawn Elements Logic
+// doesn't get faster, just more elements spawned per second
+
+function dispatchObject() {
+    let conveyor = document.getElementById("conveyor-belt");
+    score = 0;
+    while (score < 100) {
+    setTimeout(() => {
+            // if (score < 25) {
+                let obj = spawnObject();
+                conveyor.appendChild(obj)
+                myMove(obj);
+                score = 100;
+            // }
+            // else if (score < 50) {
+        
+            // }
+            // else if (score < 75) {
+        
+            // }
+            // else {
+        
+            // }
+        }, 200);
+        score += 5;
+        console.log(score);
+    }
+}
+
+function spawnObject() {
+    // TODO: random cat/dog object logic here
+    var obj = document.createElement('div');
+    obj.classList.add("element");
+    return obj
+}
+
+function myMove(elem) {
+    const screenWidth = window.screen.width;
+    let id = null;
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+      if (pos >= screenWidth - 10) {
+        console.log("end of the screen");
+        clearInterval(id);
+      } else {
+        pos = pos + 2;
+        elem.style.left = pos + 'px';
+      }
+    }
+  }
